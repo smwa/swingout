ccdb3731e215dca8d0cfe012ea72b94b1f5e05f6
Michael Smith <michael.smith.ok@gmail.com>
2021-01-17 22:18:17 -0600

    Usability

--- swingout/communities/urls.py
+++ swingout/communities/urls.py
@@ -6,6 +6,7 @@ app_name = 'communities'
 urlpatterns = [
     path('index.html', views.map, name='map'),
     path('', views.map, name='map'),
+    path('manifest.json', views.manifest, name='manifest'),
     path('api/communities', views.index, name='index'),
     path('communities/add', views.add, name='add'), # Only used for reverse url lookups
     path('communities/add/<latitude>/<longitude>', views.add, name='addWithCoordinates'),
